be23a71f3a34af2ca6d8f1c8935b89e798f95f43
unknown <Christopher201299@gmail.com>
2023-09-14 18:19:06 -0500

    hola

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NAVBARComponent } from './componentes/navbar/navbar.component';
+import { GaleriaComponent } from './componentes/pages/galeria/galeria.component';
+import { DatosComponent } from './componentes/pages/datos/datos.component';
+import { InicioComponent } from './componentes/pages/inicio/inicio.component';
+import { MapaComponent } from './componentes/pages/mapa/mapa.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NAVBARComponent,
+    GaleriaComponent,
+    DatosComponent,
+    InicioComponent,
+    MapaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
