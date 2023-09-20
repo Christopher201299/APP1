7e14caccb642c34408b85b99fab8c6e14781455c
unknown <Christopher201299@gmail.com>
2023-09-20 18:00:15 -0500

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/pages/inicio/inicio.component';
+import { GaleriaComponent } from './componentes/pages/galeria/galeria.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'Inicio',component:InicioComponent},
+{path:'Galeria',component:GaleriaComponent},{path:'**',component:InicioComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
